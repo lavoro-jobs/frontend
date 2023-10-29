375e062c138f8660b2a95e3edbb70fe1efb65349
Gabriela Oroz <gabriela.oroz@fer.hr>
2023-10-29 16:27:14 +0100

    Setup frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/shared/Header'
+import Footer from '@/components/shared/Footer'
+
+export default function Root() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
