292a38fdf6cd391e8f60f542dd9f6dc7061c6b92
Marko Bolt <marko.bolt@fer.hr>
2023-11-16 02:04:09 +0100

    removed unused imports and files

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,9 @@
 
 import Header from "@/components/shared/Header"
 import Footer from "@/components/shared/Footer"
-import { usePathname, useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
 import useProtectedRoute from "@/hooks/useProtectedRoute"
-import { Role } from "@/types/User"
+import { Role } from "@/types/Auth"
 import { Spinner } from "@chakra-ui/react"
 
 export default function Profile() {
--- src/types/User.ts
+++
@@ -1,4 +0,0 @@
-export enum Role {
-  APPLICANT = "applicant",
-  RECRUITER = "recruiter",
-}
