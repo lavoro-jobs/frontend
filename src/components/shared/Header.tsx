76725d183a6f3fb859b7b6109db62aecb6aec8b0
Gabriela Oroz <gabriela.oroz@fer.hr>
2023-12-05 22:14:57 +0100

    Fixed conflict

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -14,40 +14,40 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header({ currentRoute }: { currentRoute?: string }) {
-	return (
-		<header>
-			<Flex h="100%" justify="space-between" align="stretch">
-				<Link href="/">
-					<h1 className="header-title">Lavoro</h1>
-				</Link>
-				<nav>
-					<div className="nav-esthetic"></div>
-					<Flex gap="24px" align="center">
-						<Link href="/">
-							<Text fontSize="lg" as={currentRoute === "/home" ? "b" : "p"} color="#fff">
-								Home
-							</Text>
-						</Link>
-						<Link href="/job-posts">
-							<Text fontSize="lg" as={currentRoute === "/job-posts" ? "b" : "p"} color="#fff">
-								Job posts
-							</Text>
-						</Link>
-						<Link href="/about-us">
-							<Text fontSize="lg" as={currentRoute === "/about-us" ? "b" : "p"} color="#fff">
-								About us
-							</Text>
-						</Link>
-					</Flex>
+  return (
+    <header>
+      <Flex h="100%" justify="space-between" align="stretch">
+        <Link href="/">
+          <h1 className="header-title">Lavoro</h1>
+        </Link>
+        <nav>
+          <div className="nav-esthetic"></div>
+          <Flex gap="24px" align="center">
+            <Link href="/">
+              <Text fontSize="lg" as={currentRoute === "/home" ? "b" : "p"} color="#fff">
+                Home
+              </Text>
+            </Link>
+            <Link href="/job-posts">
+              <Text fontSize="lg" as={currentRoute === "/job-posts" ? "b" : "p"} color="#fff">
+                Job posts
+              </Text>
+            </Link>
+            <Link href="/about-us">
+              <Text fontSize="lg" as={currentRoute === "/about-us" ? "b" : "p"} color="#fff">
+                About us
+              </Text>
+            </Link>
+          </Flex>
 
-					<ButtonGroup>
-						<Button as="a" href="/signin" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
-							Sign in
-						</Button>
-						<Button as="a" href="/signup" bgColor="#FF8E2B" _hover={{ bgColor: "#fdb16e" }} color="#0D2137" h="32px">
-							Sign up
-						</Button>
-					</ButtonGroup>
+          <ButtonGroup>
+            <Button as="a" href="/signin" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
+              Sign in
+            </Button>
+            <Button as="a" href="/signup" bgColor="#FF8E2B" _hover={{ bgColor: "#fdb16e" }} color="#0D2137" h="32px">
+              Sign up
+            </Button>
+          </ButtonGroup>
 
           <Menu>
             <MenuButton
