c0c9ab3cdd64502fe7a8a0183279b84ea38568bb
Maksim <madzarmaksim@gmail.com>
2023-12-13 18:15:50 +0100

    fix applicant not rerouting

--- src/app/profile-setup/page.tsx
+++ src/app/profile-setup/page.tsx
@@ -1,32 +1,66 @@
-    "use client";
+"use client";
 
-    import ApplicantProfileSetup from "@/components/features/profileSetup/ApplicantProfileSetup";
-    import { Role } from "@/types/Auth";
-    import RecruiterProfileSetup from "@/components/features/profileSetup/RecruiterProfileSetup";
-    import useProtectedRoute from "@/hooks/useProtectedRoute";
-    import getApplicantProfile from "@/helpers/getApplicantProfile";
-    import getRecruiterProfile from "@/helpers/getRecruiterProfile";
-    import { useRouter } from "next/navigation";
+import ApplicantProfileSetup from "@/components/features/profileSetup/ApplicantProfileSetup";
+import {Role} from "@/types/Auth";
+import RecruiterProfileSetup from "@/components/features/profileSetup/RecruiterProfileSetup";
+import useProtectedRoute from "@/hooks/useProtectedRoute";
+import getApplicantProfile from "@/helpers/getApplicantProfile";
+import getRecruiterProfile from "@/helpers/getRecruiterProfile";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+import getCurrentRecruiterProfile from "@/helpers/getCurrentRecruiterProfile";
 
-    export default function Profile() {
-      const { auth } = useProtectedRoute([Role.APPLICANT, Role.RECRUITER]);
-      const router = useRouter();
+export default function Profile() {
+  const {auth} = useProtectedRoute([Role.APPLICANT, Role.RECRUITER]);
+  const router = useRouter();
 
-      if (auth?.role == Role.APPLICANT) {
-        getApplicantProfile().then((resp) => {
-          router.push("/dashboard");
-        }).catch((err) => {
-          if (error.response && error.response.status === 404) {
-            return <ApplicantProfileSetup></ApplicantProfileSetup>;
-          }
-        });
-      } else if (auth?.role == Role.RECRUITER) {
-        getRecruiterProfile().then((resp) => {
-          router.push("/dashboard");
-        }).catch((err) => {
-          if (error.response && error.response.status === 404) {
-            return <RecruiterProfileSetup></RecruiterProfileSetup>;
-          }
-        });
+  useEffect(() => {
+    const navigateTo = (path: string) => router.push(path);
+    const isProfileSetupNeeded = (profile: any) => profile == null || profile?.first_name === null;
+
+    const handleRecruiterProfile = async () => {
+      try {
+        const profile = await getCurrentRecruiterProfile();
+        if (!isProfileSetupNeeded(profile)) {
+          navigateTo("/dashboard")
+        } else {
+          return <RecruiterProfileSetup></RecruiterProfileSetup>;
+        }
+      } catch (error) {
+        // Navigating to profile-setup if the recruiter is not in the db
+        navigateTo("/profile-setup");
       }
-    }
+    };
+
+    const handleApplicantProfile = async () => {
+      try {
+        const profile = await getApplicantProfile();
+        if (!isProfileSetupNeeded(profile)) {
+          navigateTo("/dashboard")
+        } else {
+          return <ApplicantProfileSetup></ApplicantProfileSetup>;
+        }
+      } catch (error) {
+        // Navigating to profile-setup if the recruiter is not in the db
+        navigateTo("/profile-setup");
+      }
+    };
+
+    const checkProfile = async () => {
+      if (!auth) return;
+      if (auth.role === Role.RECRUITER) {
+        await handleRecruiterProfile();
+      } else if (auth.role === Role.APPLICANT) {
+        await handleApplicantProfile();
+      }
+    };
+    checkProfile();
+  }, [auth, router]);
+
+  if (auth?.role == Role.APPLICANT) {
+    return <ApplicantProfileSetup></ApplicantProfileSetup>;
+  } else if (auth?.role == Role.RECRUITER) {
+    return <RecruiterProfileSetup></RecruiterProfileSetup>;
+  }
+}
+
