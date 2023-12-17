b23ba8de4e01c08c11a1c0801070c46ad1b81f97
antevujcic <ante.vujcic@fer.hr>
2023-12-17 11:09:08 +0100

    - Created About-us - Changed footer

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,10 @@
 "use client";
-
+import {
+  Heading,
+  Highlight,
+  Button,
+  Flex
+} from "@chakra-ui/react";
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 import { usePathname } from "next/navigation";
@@ -8,43 +13,39 @@ export default function AboutUs() {
   return (
     <>
       <Header currentRoute={usePathname()} />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem numquam officia optio cupiditate obcaecati
-        alias dicta enim minima ad natus mollitia quibusdam eligendi porro, rem perspiciatis animi. Veritatis dolores
-        harum at blanditiis provident fugiat atque. Architecto officiis at sed. Unde id eius, sapiente excepturi
-        dolores, nobis quae cumque sequi ea consequatur nesciunt iure placeat quod, voluptas dolore quis quidem? Quae
-        doloremque aspernatur laudantium accusantium cum incidunt corporis, quibusdam obcaecati inventore sed quidem
-        molestias illum voluptatum facilis soluta harum mollitia dolore. Aperiam fugiat vero autem sit deleniti numquam
-        natus enim ratione. Fugiat, temporibus dignissimos accusamus laborum rem quas molestiae suscipit quaerat ad
-        distinctio possimus harum reiciendis pariatur tempore saepe praesentium officia quia aspernatur esse, rerum eius
-        asperiores, excepturi voluptatibus. Necessitatibus at dicta perferendis voluptatibus minus alias modi porro,
-        cupiditate, reiciendis dignissimos et quo quisquam natus sunt a ipsa omnis illum recusandae esse aliquid
-        corrupti! Perferendis alias quod architecto, consectetur magnam ipsa ab eos nobis vitae quae, voluptates sit
-        commodi veniam iste aliquid recusandae reiciendis sequi inventore atque nihil fugit repellat voluptate
-        veritatis. Aliquam assumenda eius cum quo accusantium veritatis voluptatibus eum. Ex esse provident aut vero
-        fuga iure excepturi consectetur maxime fugit nesciunt vel impedit tenetur pariatur neque, repellat commodi magni
-        non ut nostrum laboriosam, similique a atque eius. Beatae, enim nihil? Necessitatibus similique, a iure
-        corrupti, eum ad minima quidem nisi sed quos rerum cum, ipsam accusamus omnis fugit. Iste, harum eveniet, saepe
-        temporibus ullam aliquam reiciendis eligendi cum architecto, nobis corporis! Tenetur, dolores? Rerum inventore
-        porro accusantium. Harum ut et veniam, nihil optio, eligendi impedit aliquid fuga iusto, repellat perspiciatis
-        quaerat nemo quidem assumenda asperiores fugit neque consequatur autem delectus quasi aspernatur obcaecati?
-        Quae, quos dicta, libero ipsa unde accusamus nesciunt eaque sequi illum explicabo, architecto temporibus commodi
-        deleniti mollitia. Illo sit explicabo sint numquam deserunt, odit ut quisquam voluptatem nulla, fugit fuga.
-        Aperiam beatae laboriosam dolorem possimus harum reiciendis. Adipisci delectus ad repellat molestias. Quibusdam
-        veniam dolores culpa? Sint, corporis eveniet? Magnam culpa voluptas laboriosam quia impedit fuga aliquid ullam
-        cumque sit ad! Aspernatur amet maxime est labore nesciunt dolorum esse quos temporibus. Consequatur tenetur
-        quidem necessitatibus deleniti omnis soluta atque ab quae magni, esse sequi incidunt voluptates sit mollitia
-        molestiae, ipsa deserunt explicabo fugiat voluptatum temporibus eaque architecto beatae sint! Ducimus cumque
-        cupiditate explicabo aspernatur accusamus. Perferendis alias, assumenda aliquid voluptatibus, fugiat iusto nihil
-        eius non nulla hic quae ipsa voluptates, deserunt tempore molestias est commodi aut facere nemo. Illum, fuga!
-        Reprehenderit iure vitae temporibus dicta quasi? Atque animi eligendi dolorum aspernatur veniam nostrum,
-        distinctio impedit id officiis ab numquam perspiciatis, facere autem. Cupiditate possimus sed ab, est eaque,
-        voluptatum officia impedit praesentium sint animi voluptate dolorum neque! Sequi, dignissimos at corporis soluta
-        ipsum animi autem. Eligendi ea saepe laudantium hic vitae impedit excepturi illo quam nam pariatur id animi,
-        minus cum molestiae illum tempore ipsam maxime fugiat itaque! Magnam saepe qui nostrum aut expedita libero minus
-        doloremque quasi velit dolores natus dignissimos, ratione ullam, id alias officiis adipisci rem ea itaque
-        aliquam quod voluptatum debitis veniam.
+      <Heading fontSize="6xl" lineHeight='tall' paddingTop="150px" w="fit-content" margin="0 auto 20px">
+        <Highlight
+          query={['your place']}
+          styles={{ px: '2', py: '1', rounded: 'full', bg: '#0071c6', color: "#fff" }}
+        >
+          We've been in your place
+        </Highlight>
+      </Heading>
+      <div className="about-us-subtitle">
+          <h2>
+            After years of experience, we've seen it all!
+            <br/>
+            From bad matching algorithms to useless job posts. It's time to make a difference.
+          </h2>
+          <Button as="a" href="/job-posts" bgColor="#FF8E2B" _hover={{ bgColor: "#fdb16e" }} color="#0D2137" h="32px">
+            See our job posts
+          </Button>
       </div>
+
+      <Flex  className="about-us-details" alignItems="flex-start" justifyContent="space-between" w="80%" margin="0 auto 100px">
+        <div>
+            <Heading className="secondary-heading" fontSize="4xl" lineHeight='tall'  w="fit-content" margin="0 0 20px">
+                <Highlight
+                  query={['project']}
+                  styles={{ px: '2', py: '1', rounded: 'full', bg: '#0071c6', color: "#fff" }}
+                >
+                  Started as a project
+                </Highlight>
+            </Heading>
+            <p>Originating as a distinguished project incubated at FER, University of Zagreb, Lavoro embodies the fusion of academic rigor and entrepreneurial vision.<br/><br/>Our roots trace back to the esteemed halls of the university, where our journey commenced as an ambitious endeavor fueled by innovation and driven by the quest for redefining the recruitment landscape.<br/><br/>From humble beginnings as a university project, we've evolved into a pioneering force in the software development recruitment sphere, leveraging the knowledge, expertise, and dedication fostered within the academic corridors to shape a platform that harmonizes talent and opportunity on a global scale.</p>
+        </div>
+        <img src="/assets/images/team.jpg"/>
+      </Flex>
       <Footer />
     </>
   );
