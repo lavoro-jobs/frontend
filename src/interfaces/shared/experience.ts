045a67d313a21333e5ec191fb1f3d6eca96f6770
Maksim <madzarmaksim@gmail.com>
2023-12-19 16:13:24 +0100

    add position to Experience interface

--- src/interfaces/shared/experience.ts
+++ src/interfaces/shared/experience.ts
@@ -1,4 +1,10 @@
+interface Position {
+  id: string;
+  position_name: string;
+}
+
 export default interface Experience {
+  position?: Position;
   company_name?: string;
   position_id?: number;
   years?: number;
