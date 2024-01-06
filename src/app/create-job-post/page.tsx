93f7dfea61a6358a0e58ee2ed9f753ca52947814
Marko Bolt <marko.bolt@fer.hr>
2024-01-06 23:26:53 +0100

    deployment

--- src/app/create-job-post/page.tsx
+++ src/app/create-job-post/page.tsx
@@ -1,13 +1,15 @@
-"use client";
+"use client"
 
-import CreateJobPost from "@/components/features/jobPost/CreateJobPost";
-import useProtectedRoute from "@/hooks/useProtectedRoute";
-import { Role } from "@/types/Auth";
+import useProtectedRoute from "@/hooks/useProtectedRoute"
+import { Role } from "@/types/Auth"
+import dynamic from "next/dynamic"
+
+const DynamicCreateJobPost = dynamic(() => import("@/components/features/jobPost/CreateJobPost"), { ssr: false })
 
 export default function JobPost() {
-  const { auth } = useProtectedRoute([Role.APPLICANT, Role.RECRUITER]);
+    const { auth } = useProtectedRoute([Role.APPLICANT, Role.RECRUITER])
 
-  if (auth?.role == Role.RECRUITER) {
-    return <CreateJobPost></CreateJobPost>;
-  }
+    if (auth?.role == Role.RECRUITER) {
+        return <DynamicCreateJobPost></DynamicCreateJobPost>
+    }
 }
