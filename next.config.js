583dc80efbada3981d584d45dde8f08e32a1d722
Marko Bolt <marko.bolt@fer.hr>
2024-01-07 11:14:20 +0100

    rewrites

--- next.config.js
+++ next.config.js
@@ -14,8 +14,8 @@ module.exports = {
         return [
             {
                 source: "/api/:path*",
-                destination: process.env.API_URL
-                    ? process.env.API_URL + "/api/:path*"
+                destination: process.env.NEXT_PUBLIC_API_URL
+                    ? process.env.NEXT_PUBLIC_API_URL + "/api/:path*"
                     : "http://localhost:8000/api/:path*",
             },
         ]
