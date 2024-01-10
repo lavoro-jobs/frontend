63519f6e70db7d035d6d80a6ee3ad22bb9ed7a55
Marko Bolt <marko.bolt@fer.hr>
2024-01-10 18:13:56 +0100

    changed some components to use ssr and added loading spinners

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,132 +1,177 @@
+"use client"
 import {
-  Button,
-  ButtonGroup,
-  Flex,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Text,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  DrawerBody,
-  DrawerHeader,
-  DrawerFooter
-} from "@chakra-ui/react";
-import Link from "next/link";
-import React, { useState } from "react";
-import useAuth from "@/hooks/useAuth";
+    Button,
+    ButtonGroup,
+    Flex,
+    IconButton,
+    Text,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    DrawerBody,
+    DrawerHeader,
+    DrawerFooter,
+} from "@chakra-ui/react"
+import Link from "next/link"
+import React, { useState } from "react"
+import useAuth from "@/hooks/useAuth"
+import SpinnerIfLoading from "./SpinnerIfLoading"
 
 export default function Header({ currentRoute }: { currentRoute?: string }) {
-  const { auth } = useAuth();
-  const [menu, setMenu] = useState<boolean>(false);
+    const { auth, loading } = useAuth()
+    const [menu, setMenu] = useState<boolean>(false)
 
-  const toggleHamby = () => {
-    setMenu(!menu);
-  }
-  return (
-    <header>
-      <Flex h="100%" justify="space-between" align="stretch">
-        <Link href="/">
-          <h1 className="header-title">Lavoro</h1>
-        </Link>
-        <nav>
-          <div className="nav-esthetic"></div>
-          <Flex gap="24px" align="center">
-            <Link href="/">
-              <Text fontSize="lg" as={currentRoute === "/home" ? "b" : "p"} color="#fff">
-                Home
-              </Text>
-            </Link>
-            <Link href="/jobs">
-              <Text fontSize="lg" as={currentRoute === "/job-posts" ? "b" : "p"} color="#fff">
-                Job posts
-              </Text>
-            </Link>
-            <Link href="/about-us">
-              <Text fontSize="lg" as={currentRoute === "/about-us" ? "b" : "p"} color="#fff">
-                About us
-              </Text>
-            </Link>
-          </Flex>
+    const toggleHamby = () => {
+        setMenu(!menu)
+    }
 
-          <ButtonGroup>
-            { !auth &&
-              <>
-              <Button as="a" href="/signin" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
-                Sign in
-              </Button>
-              <Button as="a" href="/signup" bgColor="#FF8E2B" _hover={{ bgColor: "#fdb16e" }} color="#0D2137" h="32px">
-                Sign up
-              </Button>
-              </>
-            }
-            { auth &&
-              <Button as="a" href="/dashboard" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
-                Dashboard
-              </Button>
-            }
-          </ButtonGroup>
-
-          <IconButton className="hamby" marginTop="16px" bgColor="transparent" _hover={{ bg: "transparent" }} onClick={toggleHamby} icon={<span className="material-symbols-outlined icon-menu">menu</span>}></IconButton>
-          <Drawer
-            isOpen={menu}
-            placement='right'
-            onClose={toggleHamby}
-          >
-            <DrawerOverlay />
-            <DrawerContent>
-              <DrawerCloseButton className="drawer-close"/>
-              <DrawerHeader className="drawer-title">
+    return (
+        <header>
+            <Flex h="100%" justify="space-between" align="stretch">
                 <Link href="/">
-                  <h1 className="header-title">Lavoro</h1>
+                    <h1 className="header-title">Lavoro</h1>
                 </Link>
-              </DrawerHeader>
+                <nav>
+                    <div className="nav-esthetic"></div>
+                    <Flex gap="24px" align="center">
+                        <Link href="/">
+                            <Text fontSize="lg" as={currentRoute === "/home" ? "b" : "p"} color="#fff">
+                                Home
+                            </Text>
+                        </Link>
+                        <Link href="/jobs">
+                            <Text fontSize="lg" as={currentRoute === "/job-posts" ? "b" : "p"} color="#fff">
+                                Job posts
+                            </Text>
+                        </Link>
+                        <Link href="/about-us">
+                            <Text fontSize="lg" as={currentRoute === "/about-us" ? "b" : "p"} color="#fff">
+                                About us
+                            </Text>
+                        </Link>
+                    </Flex>
 
-              <DrawerBody className="drawer-body">
-                <Link href="/">
-                  <Text fontSize="3xl" as={currentRoute === "/home" ? "b" : "p"} color="#0071c6">
-                    Home
-                  </Text>
-                </Link>
-                <Link href="/jobs">
-                  <Text fontSize="3xl" as={currentRoute === "/job-posts" ? "b" : "p"} color="#0071c6">
-                    Job posts
-                  </Text>
-                </Link>
-                <Link href="/about-us">
-                  <Text fontSize="3xl" as={currentRoute === "/about-us" ? "b" : "p"} color="#0071c6">
-                    About us
-                  </Text>
-                </Link>
-              </DrawerBody>
-            <DrawerFooter>
-              <ButtonGroup>
-                  { !auth &&
-                    <>
-                    <Button as="a" href="/signin" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
-                      Sign in
-                    </Button>
-                    <Button as="a" href="/signup" bgColor="#FF8E2B" _hover={{ bgColor: "#fdb16e" }} color="#0D2137" h="32px">
-                      Sign up
-                    </Button>
-                    </>
-                  }
-                  { auth &&
-                    <Button as="a" href="/dashboard" bgColor="#E0EAF5" _hover={{ bgColor: "gray.300" }} color="#0D2137" h="32px">
-                      Dashboard
-                    </Button>
-                  }
-              </ButtonGroup>
-            </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-        </nav>
-      </Flex>
-    </header>
-  );
+                    <ButtonGroup>
+                        <SpinnerIfLoading loading={loading}>
+                            {!auth ? (
+                                <>
+                                    <Button
+                                        as="a"
+                                        href="/signin"
+                                        bgColor="#E0EAF5"
+                                        _hover={{ bgColor: "gray.300" }}
+                                        color="#0D2137"
+                                        h="32px"
+                                    >
+                                        Sign in
+                                    </Button>
+                                    <Button
+                                        as="a"
+                                        href="/signup"
+                                        bgColor="#FF8E2B"
+                                        _hover={{ bgColor: "#fdb16e" }}
+                                        color="#0D2137"
+                                        h="32px"
+                                    >
+                                        Sign up
+                                    </Button>
+                                </>
+                            ) : (
+                                <Button
+                                    as="a"
+                                    href="/dashboard"
+                                    bgColor="#E0EAF5"
+                                    _hover={{ bgColor: "gray.300" }}
+                                    color="#0D2137"
+                                    h="32px"
+                                >
+                                    Dashboard
+                                </Button>
+                            )}
+                        </SpinnerIfLoading>
+                    </ButtonGroup>
+
+                    <IconButton
+                        className="hamby"
+                        marginTop="16px"
+                        bgColor="transparent"
+                        _hover={{ bg: "transparent" }}
+                        onClick={toggleHamby}
+                        icon={<span className="material-symbols-outlined icon-menu">menu</span>}
+                    ></IconButton>
+                    <Drawer isOpen={menu} placement="right" onClose={toggleHamby}>
+                        <DrawerOverlay />
+                        <DrawerContent>
+                            <DrawerCloseButton className="drawer-close" />
+                            <DrawerHeader className="drawer-title">
+                                <Link href="/">
+                                    <h1 className="header-title">Lavoro</h1>
+                                </Link>
+                            </DrawerHeader>
+
+                            <DrawerBody className="drawer-body">
+                                <Link href="/">
+                                    <Text fontSize="3xl" as={currentRoute === "/home" ? "b" : "p"} color="#0071c6">
+                                        Home
+                                    </Text>
+                                </Link>
+                                <Link href="/jobs">
+                                    <Text fontSize="3xl" as={currentRoute === "/job-posts" ? "b" : "p"} color="#0071c6">
+                                        Job posts
+                                    </Text>
+                                </Link>
+                                <Link href="/about-us">
+                                    <Text fontSize="3xl" as={currentRoute === "/about-us" ? "b" : "p"} color="#0071c6">
+                                        About us
+                                    </Text>
+                                </Link>
+                            </DrawerBody>
+                            <DrawerFooter>
+                                <ButtonGroup>
+                                    <SpinnerIfLoading loading={loading}>
+                                        {!auth ? (
+                                            <>
+                                                <Button
+                                                    as="a"
+                                                    href="/signin"
+                                                    bgColor="#E0EAF5"
+                                                    _hover={{ bgColor: "gray.300" }}
+                                                    color="#0D2137"
+                                                    h="32px"
+                                                >
+                                                    Sign in
+                                                </Button>
+                                                <Button
+                                                    as="a"
+                                                    href="/signup"
+                                                    bgColor="#FF8E2B"
+                                                    _hover={{ bgColor: "#fdb16e" }}
+                                                    color="#0D2137"
+                                                    h="32px"
+                                                >
+                                                    Sign up
+                                                </Button>
+                                            </>
+                                        ) : (
+                                            <Button
+                                                as="a"
+                                                href="/dashboard"
+                                                bgColor="#E0EAF5"
+                                                _hover={{ bgColor: "gray.300" }}
+                                                color="#0D2137"
+                                                h="32px"
+                                            >
+                                                Dashboard
+                                            </Button>
+                                        )}
+                                    </SpinnerIfLoading>
+                                </ButtonGroup>
+                            </DrawerFooter>
+                        </DrawerContent>
+                    </Drawer>
+                </nav>
+            </Flex>
+        </header>
+    )
 }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,55 +1,47 @@
-import { createContext, useEffect, useState, ReactNode, useCallback } from "react";
-import getCurrentUser from "@/helpers/getCurrentUser";
-import Auth from "@/types/Auth";
-import { Flex, Spinner } from "@chakra-ui/react";
+"use client"
+import { createContext, useEffect, useState, ReactNode, useCallback } from "react"
+import getCurrentUser from "@/helpers/getCurrentUser"
+import Auth from "@/types/Auth"
+import { Flex, Spinner } from "@chakra-ui/react"
 
 interface AuthContextType {
-  auth: Auth | null;
-  loading: boolean;
-  updateAuth: () => void;
+    auth: Auth | null
+    loading: boolean
+    updateAuth: () => void
 }
 
 const AuthContext = createContext<AuthContextType>({
-  auth: null,
-  loading: true,
-  updateAuth: () => {},
-});
+    auth: null,
+    loading: true,
+    updateAuth: () => {},
+})
 
 interface AuthProviderProps {
-  children: ReactNode;
+    children: ReactNode
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [auth, setAuth] = useState<Auth | null>(null);
-  const [loading, setLoading] = useState(true);
+    const [auth, setAuth] = useState<Auth | null>(null)
+    const [loading, setLoading] = useState(true)
 
-  const fetchAuth = useCallback(async () => {
-    setLoading(true);
-    try {
-      const { data } = await getCurrentUser();
-      const user: Auth = data;
-      setAuth(user);
-    } catch (err) {
-      setAuth(null);
-    }
-    setLoading(false);
-  }, []);
+    const fetchAuth = useCallback(async () => {
+        setLoading(true)
+        try {
+            const { data } = await getCurrentUser()
+            const user: Auth = data
+            setAuth(user)
+        } catch (err) {
+            setAuth(null)
+        }
+        setLoading(false)
+    }, [])
 
-  useEffect(() => {
-    fetchAuth();
-  }, []);
+    useEffect(() => {
+        fetchAuth()
+    }, [])
 
-  return (
-    <AuthContext.Provider value={{ auth, loading, updateAuth: fetchAuth }}>
-      {loading && (
-        <Flex height="100vh" align={"center"} justifyContent={"center"}>
-          <Spinner size="xl" />
-        </Flex>
-      )}
-      {!loading && children}
-    </AuthContext.Provider>
-  );
-};
+    return <AuthContext.Provider value={{ auth, loading, updateAuth: fetchAuth }}>{children}</AuthContext.Provider>
+}
 
-export default AuthContext;
-export type { AuthContextType };
+export default AuthContext
+export type { AuthContextType }
